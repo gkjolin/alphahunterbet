24f0b83723607b05e619eac72f5ab4a69302d799
yanoty3d <vzb10326@me.com>
2015-08-23 18:52:43 +0900

    #15 #16 Google Driveのを持ってきた。終了処理、ライフ、時間の表示。 全画面にしないとライフは表示されない。スライダーのハンドルが残っているのは不自然。終了した時の画面が不自然（古い処理が残ってる？） 集めるべきアルファベットの表示ができていない。

---
+++ AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/Manager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+	// Playerプレハブ
+	public GameObject player;
+	// タイトル
+	private GameObject title;
+	public GameObject p1;
+	
+	void Start ()
+	{
+		// Titleゲームオブジェクトを検索し取得する
+		title = GameObject.Find ("Title");
+	}
+	
+	void Update ()
+	{
+			Debug.Log ("IsPlaying()" +IsPlaying ());
+		
+		// ゲーム中ではなく、Xキーが押されたらtrueを返す。
+		if (IsPlaying()== false) {
+			//GameStart ();
+			title.SetActive (true);
+			Application.LoadLevel("stage_clear");
+		}
+	}
+	
+	void GameStart ()
+	{
+		// ゲームスタート時に、タイトルを非表示にしてプレイヤーを作成する
+		title.SetActive (false);
+		GameObject gameObjectRef = Instantiate (player, player.transform.position, player.transform.rotation) as GameObject;
+		gameObjectRef.GetComponent<Player> ().Initialize ();
+		Application.LoadLevel("stage_clear");
+	}
+	
+	public void GameOver ()
+	{
+		// ゲームオーバー時に、タイトルを表示する
+		title.SetActive (true);
+	}
+	
+	public bool IsPlaying ()
+	{
+		// ゲーム中かどうかはタイトルの表示/非表示で判断する
+		return title.activeSelf == false;
+	}
+}
---
+++ AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/TimeScript.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeScript : MonoBehaviour {
+	private float time = 300;
+	//public ballScript BallScript;
+	//********** 追記 **********// 
+	public GameObject exchangeButton;
+	public GameObject gameOverText;
+	public GameObject p1;
+	private GameObject title;
+	public GameObject player;
+	public bool test;
+	Title gvTltle;
+
+	void Start () {
+		//********** 追記 **********// 
+		gameOverText.SetActive(false);
+		//********** 追記 **********// 
+		GetComponent<Text>().text = ((int)time).ToString();
+	}
+	
+	void Update ()
+	{
+		time -= Time.deltaTime;
+		//********** 追記 **********// 
+		if (time < 0) {
+			StartCoroutine("GameOver");
+		}
+		//********** 追記 **********// 
+		if (time < 0) time = 0;
+		GetComponent<Text> ().text = ((int)time).ToString ();
+		// ゲーム中ではなく、Xキーが押されたらtrueを返す。
+	}
+	//********** 追記 **********// 
+	IEnumerator GameOver () {
+		gameOverText.SetActive(true);
+		Debug.Log ("gameover");
+		p1 = GameObject.Find ("Player");
+		Destroy (p1);
+
+		if (Input.GetKeyDown (KeyCode.X)) {
+			Application.LoadLevel("base_test");
+		}
+
+		exchangeButton.GetComponent<Button>().interactable = false;
+		//BallScript.isPlaying = false;
+		yield return new WaitForSeconds(2.0f);
+		if (Input.GetMouseButtonDown (0)) {
+			Application.LoadLevel ("title");
+
+		}
+	}
+
+
+	
+
+	
+	public void GameOver1 ()
+	{
+		// ゲームオーバー時に、タイトルを表示する
+		title.SetActive (true);
+	}
+
+
+
+	//********** 追記 **********// 
+}
