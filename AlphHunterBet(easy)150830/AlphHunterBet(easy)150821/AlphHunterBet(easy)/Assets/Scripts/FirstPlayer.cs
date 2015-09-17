e8bbf5782d670a0708b91e2393f1a1f5910a01c8
yanoty3d <vzb10326@me.com>
2015-09-17 16:34:43 +0900

    FirstPlayer.csの機能の多くをStateMachineに移譲させた。FirstPlayerにはデータとイベント処理（Collision関連）だけを残し、State固有の処理は各StateのStateMachineBehaviourに任せることにした。

--- AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/FirstPlayer.cs
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/FirstPlayer.cs
@@ -4,18 +4,17 @@ using System.Collections.Generic;
 
 public class FirstPlayer : MonoBehaviour
 {
-	
-	Spaceship Spaceship;
-	Enemy Enemy;
-	Queue<string> Queue = new Queue<string>(){};
-	DOOR DoorRefference;
-	string Answer;
-	HpBarCtrl HitPoint;
+
+
+	Animator animator;
+
+	public string Answer;
 	public int AnswerNumber = 0;
-	char_count CharacterCount;
 	public int Number;
 	public static int StageNumber;
-	StageControl StageLoad;
+
+	public Collider2D collisionData;
+	public Queue<string> Queue = new Queue<string>(){};
 
 	void Start ()
 	{
@@ -24,67 +23,37 @@ public class FirstPlayer : MonoBehaviour
 	
 	void Update ()
 	{
-		ComonPlayer.MovePlayer(Spaceship);
+		//ComonPlayer.MovePlayer(Spaceship);
 	}
 	
 	public void Initialize()
 	{
-		Spaceship = GetComponent<Spaceship> ();
-		DoorRefference = GameObject.Find ("Manager1").GetComponent<SaveDoor>().DOOR.GetComponent<DOOR>();
-		Answer = GameObject.Find ("Manager1").GetComponent<SaveDoor> ().answer;
-		HitPoint = GameObject.Find ("HpBarCtrl").GetComponent<HpBarCtrl> ();
-		CharacterCount = GameObject.Find ("char_count").GetComponent<char_count> ();
-		StageLoad = GameObject.Find ("Manager1").GetComponent<StageControl> ();
-
+//		Spaceship = GetComponent<Spaceship> ();
+//		DoorRefference = GameObject.Find ("Manager1").GetComponent<SaveDoor>().DOOR.GetComponent<DOOR>();
+//		Answer = GameObject.Find ("Manager1").GetComponent<SaveDoor> ().answer;
+//		HitPoint = GameObject.Find ("HpBarCtrl").GetComponent<HpBarCtrl> ();
+//		CharacterCount = GameObject.Find ("char_count").GetComponent<char_count> ();
+//		StageLoad = GameObject.Find ("Manager1").GetComponent<StageControl> ();
+		animator = GameObject.Find ("Player").GetComponent<Animator> ();
 	}
 	
 	public void OnTriggerEnter2D (Collider2D c)
 	{
-		
+
+		collisionData = c;
+
+		if (c.gameObject.GetComponent<Enemy> ().alphabet == "DOOR") {
+			animator.SetTrigger ("CollisionWithDoor");
+			return;
+		}
+
 		string layerName = LayerMask.LayerToName(c.gameObject.layer);
-		
+
 		if(layerName == "Enemy")
 		{
-			Queue.Enqueue(c.gameObject.GetComponent<Enemy>().alphabet);
-			string arrayQueue=string.Concat(Queue.ToArray());
-			Spaceship.Explosion();
+			animator.SetTrigger ("CollisionWithEnemy");
 
-			Destroy (c.gameObject);
-
-			if (arrayQueue != Answer)
-			{
-				if(c.gameObject.GetComponent<Enemy> ().alphabet != "DOOR")
-					HitPoint.decrease_hp();
-			}else
-			{
-				Number = CharacterCount.decrease_cnt();
-				Debug.Log (Number);
-			}
-
-			if(Queue.Count < Answer.Length)
-			{
-				return;
-			}
-			QueueFull(c,arrayQueue);
 		}
 	}
-	void QueueFull(Collider2D c,string arrayQueue){
-		Queue.Dequeue ();
-		if (arrayQueue == Answer)
-		{		
-			AnswerNumber++;
-			if(Number < 2)
-			{
-				Debug.Log ("Collected the answer");
-				DoorRefference.SetActive ();
-				
-			}
-		}
-		if (c.gameObject.GetComponent<Enemy> ().alphabet == "DOOR") {
-			CharacterCount.full_cnt();
-			Destroy (gameObject);
-			//次のステージをロード
-			StageNumber = StageLoad.NextStage(StageNumber);
-		}
-	}
+
 }
---
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/StateMachine/EnemyCollision.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyCollision : StateMachineBehaviour {
+
+	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
+		FirstPlayer firstPlayer = GameObject.Find ("Player").GetComponent<FirstPlayer> ();
+		Spaceship spaceship = GameObject.Find ("Player").GetComponent<Spaceship> ();
+
+		Collider2D c = firstPlayer.collisionData;
+		Queue<string> queue = firstPlayer.Queue;
+		string Answer = firstPlayer.Answer;
+		int Number = firstPlayer.Number;
+		HpBarCtrl HitPoint = GameObject.Find ("HpBarCtrl").GetComponent<HpBarCtrl> ();
+		char_count CharacterCount = GameObject.Find ("char_count").GetComponent<char_count> ();;
+
+		queue.Enqueue(c.gameObject.GetComponent<Enemy>().alphabet);
+		string arrayQueue=string.Concat(queue.ToArray());
+
+		spaceship.Explosion();
+		
+		Destroy (c.gameObject);
+		
+		if (arrayQueue != Answer)
+		{
+			if(c.gameObject.GetComponent<Enemy> ().alphabet != "DOOR")
+				HitPoint.decrease_hp();
+		}else
+		{
+			Number = CharacterCount.decrease_cnt();
+			Debug.Log (Number);
+		}
+		
+		if(queue.Count < Answer.Length)
+		{
+			return;
+		}
+
+		queue.Dequeue ();
+
+		if (arrayQueue == Answer)
+		{		
+			firstPlayer.AnswerNumber++;
+			if(Number < 2)
+			{
+				Debug.Log ("Collected the answer");
+				DOOR DoorRefference = GameObject.Find ("Manager1").GetComponent<SaveDoor>().DOOR.GetComponent<DOOR>();
+				DoorRefference.SetActive ();
+				
+			}
+		}
+		animator.SetTrigger("EnemyCollisionFinished");
+
+	}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
