40c24d31af832dc6ea27f320c9d595f4bd123bd2
oitsmalab <oitsmalab@gmail.com>
2015-09-30 15:03:07 +0900

    Queueの中身を敵に当たったらやり直しするように変更致しました。

--- AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/AlphabetQueueHandler.cs
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/AlphabetQueueHandler.cs
@@ -3,30 +3,42 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class AlphabetQueueHandler : IAlphabetQueueHandler {
-
+	
 	Queue<string> alphabetQueue = new Queue<string>{};
-
+	
 	int sizeOfQueue = 1;
-
+	string Answer = GameObject.Find("Manager1").GetComponent<SaveDoor>().answer;
+	
 	public Queue<string> AlphabetQueue {
 		get {
 			return alphabetQueue;
 		}
 	}
-
+	
 	public void UpdateQueue(string newAlphabet){
+		
+		//キューの中身を調べる用
+		//Debug.Log(alphabetQueue.Count);
+		
 		alphabetQueue.Enqueue(newAlphabet);
-		if (alphabetQueue.Count > sizeOfQueue) {
+		
+		if(alphabetQueue.Peek () != Answer){
+			//キューが空になるまっで吐き出す
+			while(alphabetQueue.Count != 0){
+				alphabetQueue.Dequeue();
+			}
+		}
+		
+		if(alphabetQueue.Count > sizeOfQueue){
 			alphabetQueue.Dequeue ();
 		}
 	}
-
+	
 	public string GetQueueString(){
 		return string.Concat (alphabetQueue.ToArray ());
 	}
-
+	
 	public bool ValidateQueue(string answerString,int repeats){
 		return false;
 	}
-
 }
