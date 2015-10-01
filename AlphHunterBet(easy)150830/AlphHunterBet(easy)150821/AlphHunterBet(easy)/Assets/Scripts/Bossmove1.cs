9802de09365953fab8813594e8ea6cd339f8e1eb
oitsmalab <oitsmalab@gmail.com>
2015-10-01 18:37:26 +0900

    ボスのグラフィック、爆発用のパーティクルを作成しました ボスの移動は、左方向に無限で動きます

---
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/Bossmove1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bossmove1 : MonoBehaviour {
+	
+	// Spaceshipコンポーネント
+	Spaceship spaceship;
+	
+	void Start ()
+	{
+		
+		// Spaceshipコンポーネントを取得
+		spaceship = GetComponent<Spaceship> ();
+		
+		// スタート時左に移動
+		spaceship.Move (transform.right * -1);
+	}
+}
