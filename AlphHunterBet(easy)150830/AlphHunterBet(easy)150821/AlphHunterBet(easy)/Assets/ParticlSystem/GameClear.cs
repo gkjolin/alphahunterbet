806b775876e045a0f5da1da04b4213d233acf1b9
oitsmalab <oitsmalab@gmail.com>
2015-10-01 19:03:17 +0900

    コード内容の追加記述。ほかのコードに干渉している部分は一切ありません。

--- AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/ParticlSystem/GameClear.cs
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/ParticlSystem/GameClear.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class GameClear : MonoBehaviour {
 
+	//インスペクターからGameObjectは設定する
+	// charge->field->burst
+	public GameObject[] particlObj;//particlSystemを持つObj
+	public ParticleSystem[] clearParticl;//particlSystemを追加する
+
 	// Use this for initialization
 	void Start () {
-	
+		int i = 0;
+		while (i<3) {
+			clearParticl[i]=particlObj[i].GetComponent<ParticleSystem>();
+			i++;
+		}		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+	IEnumerator clearSystem{
+
+
+	}
+
 }
