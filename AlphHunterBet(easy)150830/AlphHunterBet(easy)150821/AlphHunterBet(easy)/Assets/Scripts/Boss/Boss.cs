f1f3e9cad60174f340e23d20c2a703467cb360b2
oitsmalab <oitsmalab@gmail.com>
2015-10-02 15:59:34 +0900

    ボスが規則的に動くようなコードを新しく書きました。(BosMove.sc) そのため、Boss.csの内容に少し変更を加えています。

--- AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/Boss/Boss.cs
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/Boss/Boss.cs
@@ -11,19 +11,32 @@ public class Boss : MonoBehaviour
 	public GameObject alphabet03;
 	//アルファベットの種類の数を保存すする変数
 	public int cntAlphabet = 3;
+	//BossMove呼び出し変数
+	BossMove bs;
+
+	void LoadComponents(){
+		//読み込み
+		bs = GameObject.Find ("Boss").GetComponent<BossMove> ();
+	}
 
 	IEnumerator Start()
 	{
+		LoadComponents ();
 		//randomの返却値を保存する変数.
 		int rnd;
 		Debug.Log ("into while");
 
 		while (true) {
 			//ゲームが始まってすぐに発射されるのを防ぐために待ち時間設定.
-			yield return new WaitForSeconds (2.5f);
+			yield return new WaitForSeconds (0.5f);
 
 			//発射するものを無作為に決定するため,ランダム関数を使用.
 			rnd = Random.Range (1, cntAlphabet  + 1);
+
+			//発射前に一時停止
+			//移動しないようにBossMove内のcanMoveをfalseに.
+			bs.ChangeFlagMove(false);
+			yield return new WaitForSeconds (1.0f);
 
 			//発射するオブジェクトを決定.
 			//インスタンス化
@@ -36,6 +49,10 @@ public class Boss : MonoBehaviour
 			if(rnd == 3){
 				Instantiate (alphabet03, transform.position, transform.rotation);
 			}
+
+			//発射ののちに再び動き出す.
+			yield return new WaitForSeconds (1.0f);
+			bs.ChangeFlagMove(true);
 
 			//発射したあとすぐに発射しないように待ち時間を設定.
 			//待ち時間は"waitShotTime"で設定可能.
---
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/Boss/BossMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossMove : MonoBehaviour {
+
+	//ボスの移動速度を保存.
+	public float bossSpeed = 0.01f;
+	//ボスの移動の幅を保存.
+	public float moveWidth = 5;
+	//移動をするかしないかの状態を保存.
+	bool canMove = true;
+	//1flameごとに１ずつ増加する変数
+	float cnt = 1;
+
+	void Update(){
+		if (canMove == true) {
+			SystematicMove ();
+		}
+	}
+
+	//Cos関数を使い規則的な移動をする移動形態.
+	void SystematicMove(){
+		transform.position = new Vector3 (moveWidth * Mathf.Cos (cnt * bossSpeed - 90), transform.position.y);
+		cnt++;
+	}
+
+	//外部からの呼び出し関数
+	//移動するかしないかの状態を変更するための関数
+	public void ChangeFlagMove(bool flag){
+		canMove = flag;
+	}
+}
