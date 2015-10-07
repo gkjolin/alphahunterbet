473278563de050dd4715397c90cba4b61f058271
oitsmalab <oitsmalab@gmail.com>
2015-10-07 12:11:13 +0900

    不規則なボスの移動についての作業を行いました。まだ完成していません。 理由がわかりませんが、Unsystematicメソッドの動作のせいでUnityが落ちてしまいます。 Unsystematicメソッドで無限ループが発生しているのかもしれません。

--- AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/Boss/Boss.cs
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/Boss/Boss.cs
@@ -24,39 +24,61 @@ public class Boss : MonoBehaviour
 		LoadComponents ();
 		//randomの返却値を保存する変数.
 		int rnd;
-		Debug.Log (bm);
+		//ゲームが始まってすぐに発射されるのを防ぐために待ち時間設定.
+		yield return new WaitForSeconds (0.5f);
 
-		while (true) {
-			//ゲームが始まってすぐに発射されるのを防ぐために待ち時間設定.
-			yield return new WaitForSeconds (0.5f);
+		if (bm.SysMove() == true) {
+			//SystematicMove
+			Debug.Log ("MoveRele : SystematicMove");
+			while (true) {
+				//発射するものを無作為に決定するため,ランダム関数を使用.
+				rnd = Random.Range (1, cntAlphabet + 1);
 
+				//発射前に一時停止
+				//移動しないようにBossMove内のcanMoveをfalseに.
+				bm.ChangeFlagMove (false);
+				yield return new WaitForSeconds (1.0f);
+
+				//アルファベットの投下
+				DropAlphabet(rnd);
+
+				//発射ののちに再び動き出す.
+				yield return new WaitForSeconds (1.0f);
+				bm.ChangeFlagMove (true);
+
+				//発射したあとすぐに発射しないように待ち時間を設定.
+				//待ち時間は"waitShotTime"で設定可能.
+				yield return new WaitForSeconds (waitShotTime - 2.5f);
+			}
+		} else {
+			//何回移動するかを決定する変数
+			//移動回数は最大５回
+			int rndM = Random.Range (1,6);
+			Debug.Log ("rndM : " + rndM);
 			//発射するものを無作為に決定するため,ランダム関数を使用.
-			rnd = Random.Range (1, cntAlphabet  + 1);
+			rnd = Random.Range (1, cntAlphabet + 1);
+			//UnsystematicMove
+			Debug.Log ("MoveRele : UnsystematicMove");
 
-			//発射前に一時停止
-			//移動しないようにBossMove内のcanMoveをfalseに.
-			bm.ChangeFlagMove(false);
-			yield return new WaitForSeconds (1.0f);
+			//移動を実行する関数
+			StartCoroutine(bm.UnsystematicMove(rndM));
+			//アルファベットを投下
+			DropAlphabet(rnd);
+		}
+	}
 
-			//発射するオブジェクトを決定.
-			//インスタンス化
-			if(rnd == 1){
-				Instantiate (alphabet01, transform.position, transform.rotation);
-			}
-			if(rnd == 2){
-				Instantiate (alphabet02, transform.position, transform.rotation);
-			}
-			if(rnd == 3){
-				Instantiate (alphabet03, transform.position, transform.rotation);
-			}
-
-			//発射ののちに再び動き出す.
-			yield return new WaitForSeconds (1.0f);
-			bm.ChangeFlagMove(true);
-
-			//発射したあとすぐに発射しないように待ち時間を設定.
-			//待ち時間は"waitShotTime"で設定可能.
-			yield return new WaitForSeconds (waitShotTime - 2.5f);
+	//アルファベットを投下するための関数
+	void DropAlphabet(int random){
+		//発射するオブジェクトを決定.
+		//インスタンス化
+		if (random == 1) {
+			Instantiate (alphabet01, transform.position, transform.rotation);
+		}
+		if (random == 2) {
+			Instantiate (alphabet02, transform.position, transform.rotation);
+		}
+		if (random == 3) {
+			Instantiate (alphabet03, transform.position, transform.rotation);
 		}
 	}
 }
--- AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/Boss/BossMove.cs
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/Boss/BossMove.cs
@@ -7,15 +7,32 @@ public class BossMove : MonoBehaviour {
 	public float bossSpeed = 0.01f;
 	//ボスの移動の幅を保存.
 	public float moveWidth = 5;
+	//gameObjectのrendererを読み出すための変数
+	Renderer r;
 	//移動をするかしないかの状態を保存.
-	bool canMove = true;
+	bool sysMove = false;
 	//1flameごとに１ずつ増加する変数
 	float cnt = 1;
+	//見えるようになる見えなくなる速度を設定する変数
+	public float canSeeSpeed = 0.01f;
+	//見えなくなってからの時間を設定する変数
+	public float cantSeeTime = 1.5f;
+	//見えるようになってからの時間を設定する変数
+	public float canSeeTime = 2.5f;
+
+	void Start(){
+		r = gameObject.GetComponent<Renderer>();
+	}
 
 	void Update(){
-		if (canMove == true) {
+		if (sysMove == true) {
 			SystematicMove ();
 		}
+	}
+
+	//sysMoveの状態を返す.
+	public bool SysMove(){
+		return sysMove;
 	}
 
 	//Cos関数を使い規則的な移動をする移動形態.
@@ -24,9 +41,36 @@ public class BossMove : MonoBehaviour {
 		cnt++;
 	}
 
+	//不規則な移動をする移動形態.　イメージは瞬間移動
+	//仮引数Countは、何回移動するかを設定
+	public IEnumerator UnsystematicMove(int count){
+		//移動する先の座標を保存するための変数
+		float rnd_x = Random.Range (-5.0f, 5.0f);
+		float rnd_y = Random.Range (0.0f, 3.65f);
+		Debug.Log ("rndX : " + rnd_x + ", rndY : " + rnd_y);
+
+		//徐々に見えなくなっていく演出
+		while(r.material.color.a != 0){
+			r.material.color -= new Color(0, 0, 0, canSeeSpeed);
+		}
+		//gameObjectの座標をゲーム画面外へ移動(予期しない動作を防ぐ);
+		gameObject.transform.position = new Vector3 (30, 30, 0);
+
+		yield return new WaitForSeconds (cantSeeTime);
+
+		//瞬間移動先に前もって移動
+		gameObject.transform.position = new Vector3 (rnd_x, rnd_y, 0);
+		//徐々に見えてくる演出
+		while (r.material.color.a != 1) {
+			r.material.color += new Color(0, 0, 0, canSeeSpeed);
+		}
+		//移動した後の待機時間
+		yield return new WaitForSeconds (canSeeTime);
+	}
+
 	//外部からの呼び出し関数
 	//移動するかしないかの状態を変更するための関数
 	public void ChangeFlagMove(bool flag){
-		canMove = flag;
+		sysMove = flag;
 	}
 }
