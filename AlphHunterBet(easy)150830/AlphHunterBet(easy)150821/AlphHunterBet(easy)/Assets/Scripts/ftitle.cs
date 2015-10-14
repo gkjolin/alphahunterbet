d7ea481494606f1b9f5963a90f5e353424a0c014
oitsmalab <oitsmalab@gmail.com>
2015-10-14 15:53:03 +0900

    タイトル画面追加

---
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/ftitle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ftitle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				// mouce clicked
+				if (Input.GetMouseButtonDown(0)){
+						// scene load
+						Application.LoadLevel("stage_2");
+					
+				}
+		
+	}
+}
