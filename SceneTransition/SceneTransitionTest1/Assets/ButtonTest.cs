899fcd0c77ef88ccc87ae1d8b8000e1b164678ab
yanoty3d <vzb10326@me.com>
2015-10-14 18:23:11 +0900

    SceneManagerScriptをシングルトンで作ってみる。未完成。

---
+++ SceneTransition/SceneTransitionTest1/Assets/ButtonTest.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonTest : MonoBehaviour {
+
+	void OnButton () {
+		SceneManagerScript.GetAnimator ().SetTrigger ("Scene1");
+	}
+}
---
+++ SceneTransition/SceneTransitionTest1/Assets/SceneManager/Script/SceneManagerScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManagerScript : MonoBehaviour {
+
+	private static SceneManagerScript mInstance;
+	
+	private SceneManagerScript () { // Private Constructor
+		
+		Debug.Log("Create SceneManager GameObject instance.");
+	}
+		
+	public static SceneManagerScript Instance {
+			
+		get {
+			
+			if( mInstance == null ) {
+				
+				GameObject go = new GameObject("SceneManager");
+				mInstance = go.AddComponent<SceneManagerScript>();
+			}
+			
+			return mInstance;
+		}
+	}
+		
+	void Start () {
+		Debug.Log("Start");
+	}
+	
+	void Update () {
+		
+		Debug.Log("Update");
+	}
+}
