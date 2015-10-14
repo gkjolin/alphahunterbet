691adf3e548b8cf1e26efbeae0672aeff6ef7b29
yanoty3d <vzb10326@me.com>
2015-10-14 17:55:07 +0900

    SceneTransitionをAnimatorで管理するためのテストプロジェクト

---
+++ SceneTransition/SceneTransitionTest1/Assets/SceneManager/Script/BuildSceneManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildSceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		DontDestroyOnLoad(transform.gameObject);	
+	}
+}
