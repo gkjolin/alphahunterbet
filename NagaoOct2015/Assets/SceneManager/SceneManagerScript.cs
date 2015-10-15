7f97d1277cae8521d4fa9bdc1f69359ae1b74c0c
yanoty3d <vzb10326@me.com>
2015-10-15 19:48:00 +0900

    SceneManagerパッケージをいれ、テストシーンを追加。

---
+++ NagaoOct2015/Assets/SceneManager/SceneManagerScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManagerScript : MonoBehaviour {
+
+
+    public GameObject _prefab;
+    public string _startScene;
+
+    static GameObject prefab;
+    static string startScene;
+    static SceneManagerScript mInstance;
+    static Animator animator;
+
+    private SceneManagerScript () { // Private Constructor
+		
+		Debug.Log("Create SceneManager GameObject instance.");
+	}
+		
+	public static SceneManagerScript Instance {
+			
+		get {
+			if( mInstance == null ) {
+				
+				GameObject go = Instantiate(prefab,new Vector3(0,0,0),Quaternion.identity) as GameObject;
+                mInstance = go.GetComponent<SceneManagerScript>();
+                animator = go.GetComponent<Animator>();
+				DontDestroyOnLoad(go);
+				Application.LoadLevel (startScene);
+			}
+			return mInstance;
+		}
+	}
+		
+	void Start () {
+        prefab = _prefab;
+        startScene = _startScene;
+        SceneManagerScript hoge = Instance;
+	}
+	
+    public static void SetTrigger(string trigger)
+    {
+        animator.SetTrigger(trigger);
+    }
+    
+}
