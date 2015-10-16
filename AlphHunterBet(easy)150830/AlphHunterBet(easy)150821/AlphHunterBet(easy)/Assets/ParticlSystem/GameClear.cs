20048f6732e01b34fd910f379c9adf0731738c41
oitsmalab <oitsmalab@gmail.com>
2015-10-16 17:18:12 +0900

    #30　自機のパーティクル遷移のコードを一部修正。 AnimationのViewで何を次に起こすかわかりやすく名前と中身を変更。 自機が真上に飛ぶようにしましたが、Animationの都合上決まった位置から決まった場所まで動くようになっています。 プレイヤーの位置に関係なく真上に飛ばす場合はApplyRootMotionをonにすることで可能です。 また最後の飛び出しはｶｯｺよさを重視してキーフレーム上で値をいくつか管理してあるので、ほかの人にはいじりにくい状態になっています。

--- AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/ParticlSystem/GameClear.cs
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/ParticlSystem/GameClear.cs
@@ -7,11 +7,10 @@ public class GameClear : MonoBehaviour {
 	public GameObject[] particlObj;//particlSystemを持つObj
 	public ParticleSystem[] clearParticl;//particlSystemを追加する
 	public int nextParticlNumber;//実行するparticlの番号
-	public float boosterSpeed;//Booster点火後のspeed
-	int screenWidth;
-	int screenHeight;
-	Vector2 screenCenter;//画面中央
-	Vector2 startPosition;//開始場所
+	public float boosterSpeed;//Booster点火後のspeed max45
+	public float boosterSize;//Boosterの火の大きさ max1.5
+	public float boosterLifeTime;//Boosterの火の持続時間 max1.0
+	bool boosterFire=false;
 
 	public enum CLEAR{
 		NONE=-1,
@@ -27,16 +26,32 @@ public class GameClear : MonoBehaviour {
 		while (i<3) {
 			clearParticl[i]=particlObj[i].GetComponent<ParticleSystem>();
 			i++;
-		}	
+		}
 	}
-
-	//演出開始 chargeから実行する
-	public void startParticl(int particlNumber){
-		clearParticl [particlNumber].Play ();//charge演出開始
-		particlNumber++;
+	void Update(){
+		if (boosterFire == true) {
+			//boosterのspeedをアニメーションにて設定
+			clearParticl [nextParticlNumber].startSpeed = boosterSpeed;
+			clearParticl[nextParticlNumber].startSize=boosterSize;
+			clearParticl[nextParticlNumber].startLifetime=boosterLifeTime;
+		}
 	}
-	//boosterのスピードを調節
-	public void boosterControll(int particlNumber){
-		clearParticl [particlNumber].startSpeed = boosterSpeed;
+	//エネルギーを集める animationViewで引数は0を設定
+	public void startCharge(int animationEvent_Int){
+		clearParticl [animationEvent_Int].Play ();//animation開始
+		nextParticlNumber++;//次のアニメーション番号
+	}
+	//エネルギー開放 animationViewで引数は1を設定
+	public void burstForce(int animationEvent_Int){
+		clearParticl [nextParticlNumber - 1].Stop ();//前のアニメーションを停止
+		clearParticl [animationEvent_Int].Play ();//次のアニメーションを開始
+		nextParticlNumber++;//次のアニメーション番号
+	}
+	//ブースターのアニメーションはｶｯｺよく見せるためにキーフレームでspeedを調整.
+	//ブースター点火 updateでspeedを調整 animationViewで引数は2を設定
+	public void enagyBoosterFire(int animationEvent_Int){
+		clearParticl [nextParticlNumber - 1].Stop ();//前のアニメーション停止
+		clearParticl [animationEvent_Int].Play ();//次のアニメーション開始
+		boosterFire = true;//Update関数の記述を実行する.
 	}
 }
