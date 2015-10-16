fdf14d2b71a526a57c543202c9488ce832ac412a
yanoty3d <vzb10326@me.com>
2015-10-16 19:44:20 +0900

    Alphabet パッケージを入れ、各パッケージをビルド時にテストできるよう、シーン遷移を設定。

---
+++ NagaoOct2015/Assets/Alphabet/Scripts/EnemyMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour {
+	
+	Spaceship _spaceship;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().gravityScale = 1;
+	}
+	void OnBecameInvisible() {
+		Destroy (this.gameObject);
+	}
+
+	/*void OnWillRenderObject(){
+		Debug.Log(Camera.current.name);
+		if (Camera.current.name != "MainCamera") {
+			Destroy (this.gameObject);
+			Debug.Log("destroy");
+		}
+
+	}*/
+
+	// Update is called once per frame
+	void Update () {
+		Physics2D.gravity = Vector2.up * speed;
+	}
+	
+}
---
+++ NagaoOct2015/Assets/Alphabet/Scripts/Spaceship.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Spaceship : MonoBehaviour
+{	
+	// 爆発のPrefab
+	public GameObject explosion;
+	
+	// 爆発の作成
+	public void Explosion ()
+	{
+		Instantiate (explosion, transform.position, transform.rotation);
+	}
+}
