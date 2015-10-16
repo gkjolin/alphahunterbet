92702c57f8633ce50ddcb04f98fb4a7fd13514e2
oitsmalab <oitsmalab@gmail.com>
2015-10-16 17:48:52 +0900

    Textでステータスを配置しました。SceneはStage3です。

---
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/Remaining.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Remaining : MonoBehaviour {
+
+	public AlphabetQueueHandler _Queue;
+
+	void Update () {
+	
+		this.GetComponent<Text>().text = "残り:" + ;
+
+	}
+}
