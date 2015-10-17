be95b0d261c86e0b35c126a4394375f1e6fbef67
yanoty3d <vzb10326@me.com>
2015-10-17 10:52:12 +0900

    InterfaceとIUnifiedのパッケージをいれ、Testパッケージに専用のAnimation controllerをいれた。各パッケージのテストシーン自体は各パッケージの中にある。 PlayerViewをとりこみ

---
+++ NagaoOct2015/Assets/Interface/IUserInput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public interface IUserInput {
+
+	Vector2 GetInputVector ();
+	
+}
+
+[Serializable]
+public class IUserInputContainer : IUnifiedContainer<IUserInput> { }
---
+++ NagaoOct2015/Assets/PlayerView/Test/MockIUserInput.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MockIUserInput : MonoBehaviour,IUserInput {
+
+    public Vector2 GetInputVector()
+    {
+        return new Vector2(1, 0);
+    }
+
+
+}
