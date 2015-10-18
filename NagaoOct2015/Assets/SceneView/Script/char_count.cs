fc61aafa2ffc503d17a07c38b84b6abe1a807a36
yanoty3d <vzb10326@me.com>
2015-10-18 21:44:30 +0900

    SceneVieｗパッケージをインポート

---
+++ NagaoOct2015/Assets/SceneView/Script/char_count.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // ←※これを忘れずに入れる
+
+public class char_count : MonoBehaviour, IAlphabetQueueObserver
+{
+
+    public int numberToCollect = 3;
+    public int LeftToCollect
+    {
+        get
+        {
+            return numberToCollect - _IAlphabetQueueObservable.queueString.Length;
+        }
+    }
+
+    public IAlphabetQueueObservable _IAlphabetQueueObservable;
+
+    void Start()
+    {
+        _IAlphabetQueueObservable.Add(this);
+    }
+
+    public void UpdateAlphabetQueueObserver(IAlphabetQueueObservable observable)
+    {
+        GetComponent<Text>().text = (LeftToCollect).ToString();
+    }
+}
