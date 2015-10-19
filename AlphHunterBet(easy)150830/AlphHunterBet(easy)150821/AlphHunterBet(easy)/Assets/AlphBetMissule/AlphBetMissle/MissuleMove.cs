5bd7fba426a6f33f431bff5ee343aad7a056d112
oitsmalab <oitsmalab@gmail.com>
2015-10-19 18:27:56 +0900

    ファイルの整理と特定オブジェクトで爆発するように調整

---
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/AlphBetMissule/AlphBetMissle/MissuleMove.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissuleMove : MonoBehaviour {
+	public int explodeLevel = 10;//爆発規模
+	public GameObject target = null;//ターゲット
+	public float moveSpeed = 1;//速度
+	public int timeCount = 50;//時限
+
+	private bool isCollision = false;
+
+	private Detonator exp;
+	private SpriteRenderer render;
+	private AudioSource sound;
+	private PolygonCollider2D missuleCollider;
+
+	// Use this for initialization
+	void Start ()
+	{
+		exp = gameObject.GetComponent<Detonator> ();
+		render = gameObject.GetComponent<SpriteRenderer> ();
+		sound = gameObject.GetComponent<AudioSource> ();
+		missuleCollider = gameObject.GetComponent<PolygonCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update (){
+		if (isCollision || timeCount <= 0) {
+			if (explodeLevel > 0) {
+				MissuleExplode ();
+			}
+		} else {
+			gameObject.transform.Translate (0, 0.1f * moveSpeed, 0);
+			timeCount--;
+		}
+	}
+	
+	void OnCollisionEnter2D(Collision2D collision){
+		if (collision.gameObject.layer == target.layer) {
+			render.enabled = false;
+			sound.PlayOneShot (sound.clip);	
+			isCollision = true;
+		}
+	}
+
+	private void MissuleExplode(){
+		exp.MissuleExplode();
+		explodeLevel--;
+	}
+
+}
