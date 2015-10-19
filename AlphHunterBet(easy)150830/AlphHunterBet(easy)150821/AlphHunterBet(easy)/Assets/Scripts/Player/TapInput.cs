09d1c2adc68b431c7800b32972ebeb7da18d5006
oitsmalab <oitsmalab@gmail.com>
2015-10-19 18:31:06 +0900

    左右移動のボタンを作成した。base_testではうまく動作した。

--- AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/Player/TapInput.cs
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/Player/TapInput.cs
@@ -2,7 +2,37 @@
 using System.Collections;
 
 public class TapInput : MonoBehaviour,IUserInput {
+	public float speed = 2;
+	bool push =false;
+
+	public void PushDown_left(){
+		push=true;
+		speed=-2;
+	}
+
+	public void PushUp_left(){
+		push = false;
+	}
+
+	public void PushDown_right(){
+		push=true;
+		speed=2;
+	}
+
+	public void PushUp_right(){
+		push = false;
+	}
+
+	void Updata(){
+		GetInputVector ();
+	}
+
 	public Vector2 GetInputVector (){
-        return new Vector2(0,0);
+    	if (push) {
+			Vector2 direction = new Vector2 (1.0f, 0).normalized;
+			return GetComponent<Rigidbody2D> ().velocity = direction * speed;
+		} else {
+			return new Vector2 (0, 0);
+		}
 	}
 }
