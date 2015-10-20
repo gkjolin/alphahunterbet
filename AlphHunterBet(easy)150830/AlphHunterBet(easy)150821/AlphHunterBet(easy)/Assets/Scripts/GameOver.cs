59ea62cca56af77e7a4f6794ade818fcff5798b3
oitsmalab <oitsmalab@gmail.com>
2015-10-20 16:24:37 +0900

    プレハブのアルファベットのエネミーを使って、タイトル画面のタイトルを作った

---
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/GameOver.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour 
+{
+	
+	public void GameOverScreen()
+	{
+		Application.LoadLevel ("Game_Over");
+		Debug.Log ("Game_Over);
+	}
+}
