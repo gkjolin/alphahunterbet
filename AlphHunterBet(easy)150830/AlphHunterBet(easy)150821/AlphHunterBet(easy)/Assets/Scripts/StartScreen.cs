e97db571d4c4e5a4cce42cafd9d803a5479ef780
oitsmalab <oitsmalab@gmail.com>
2015-10-21 16:12:39 +0900

    Screenの名前をStartScreenへ。Alphabetに表示するprefab、Numには見つける個数を入れてください なぜかクローンがGame画面に表示されません

---
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/StartScreen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartScreen : MonoBehaviour {
+	public GameObject Alphabet;
+	public Text txt;
+	public int Num;
+	// Use this for initialization
+	void Start () {
+		Instantiate(Alphabet, this.transform.position, this.transform.rotation);
+		txt.text = "上のアルファベットを" + Num + "個集めてね"; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
