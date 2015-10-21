d9286182da82238c59a5e394565a912af281a148
yanoty3d <vzb10326@me.com>
2015-10-21 14:53:23 +0900

    Title, Explaiについて仮シーンを作る。Releaseパッケージを作り、そちらのBareBone１でゲームの流れを試作する。SceneManagerControllerForReleaseに流れがある。 TextMeshProでタイトルを作ってみた。

---
+++ NagaoOct2015/Assets/Title/ftitle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ftitle : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+				// mouce clicked
+			if (Input.GetMouseButtonDown(0)){
+            // scene load
+            Debug.Log("mouse down");
+                SceneManagerScript.SetTrigger("Explain");					
+			}
+		
+	}
+}
