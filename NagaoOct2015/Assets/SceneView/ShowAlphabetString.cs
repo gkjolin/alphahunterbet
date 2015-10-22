108be17e2afd3b41315de5f9f138e61d5960d391
yanoty3d <vzb10326@me.com>
2015-10-22 18:38:35 +0900

    Timeと数を無くして、自分が集めたアルファベットを表示させる

---
+++ NagaoOct2015/Assets/SceneView/ShowAlphabetString.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShowAlphabetString : MonoBehaviour {
+
+    AlphabetRepeatQueue AlphabetQueueString;
+    Text Text;
+	// Use this for initialization
+	void Start () {
+        AlphabetQueueString = GameObject.Find("PlayerModel").GetComponent<AlphabetRepeatQueue>();
+        Text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Text.text = AlphabetQueueString.answer;
+    }
+}
