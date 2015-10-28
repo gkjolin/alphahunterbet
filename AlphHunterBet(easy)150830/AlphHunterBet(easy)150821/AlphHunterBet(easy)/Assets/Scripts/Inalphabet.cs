ef447fceb28005f16b7c4476d8bd8bd1dcf5f08d
oitsmalab <oitsmalab@gmail.com>
2015-10-28 15:07:49 +0900

    InAlphabetは次に集めるべきアルファベットを表示するスクリプトです。Stage3のImageのAlphaBetにSpriteを入れてください。

---
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/Inalphabet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Sprites;
+
+public class Inalphabet : MonoBehaviour {
+	private Image _image;
+	public Sprite _alphaBet;
+
+	// Use this for initialization
+	void Start () {
+		_image = GameObject.Find ("Image").GetComponent<Image> ();
+		_image.sprite = _alphaBet;
+	}
+}
