9d75c8af287a2a8e0a1313713a60645fcad6ec5c
yanoty3d <vzb10326@me.com>
2015-11-02 21:40:36 +0900

    XBoxのジョイスティックに対応。

--- NagaoOct2015/Assets/PlayerView/Scripts/MultiInput.cs
+++ NagaoOct2015/Assets/PlayerView/Scripts/MultiInput.cs
@@ -15,7 +15,7 @@ public class MultiInput : MonoBehaviour,IUserInput {
 
     void Update()
     {
-        Vector2 vectorSum = buttonLeft.Result.GetInputVector() + buttonRight.Result.GetInputVector();
+        Vector2 vectorSum = buttonLeft.Result.GetInputVector() + buttonRight.Result.GetInputVector()+new Vector2(Input.GetAxisRaw("Horizontal"),0);
         vectorSum = vectorSum + new Vector2((Input.GetKey(leftKeyCode) ? -1 : 0) + (Input.GetKey(rightKeyCode) ? 1 : 0), 0);
         if (vectorSum.magnitude == 0) { currentDirection = new Vector2(0,0); return; }
         currentDirection = (currentDirection.magnitude < maxSpeed.magnitude) ? currentDirection + vectorSum.normalized*Time.deltaTime * accel : currentDirection;
