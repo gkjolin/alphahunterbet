49eb902e32c67e689d852ef9e34801a1dd3e6ee0
oitsmalab <oitsmalab@gmail.com>
2015-11-05 14:35:40 +0900

    Emitterのコードを一部修正 --投下するアルファベットに偏りがなくなるようなコードを追加.

--- NagaoOct2015/Assets/Emitter.cs
+++ NagaoOct2015/Assets/Emitter.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Emitter : MonoBehaviour {
 
     public GameObject[] alphabets;
+	public bool[] alphabetsDroped;
 
     public ICollisionObservableContainer _ICollisionObservableContainer;
 
@@ -11,17 +12,49 @@ public class Emitter : MonoBehaviour {
     // Use this for initialization
     GameObject alphabetObject;
 
-    IEnumerator Start () {
+	public int cnt = 0;
+
+	IEnumerator Start () {
+		SetArray ();
 
         while (1 > 0)
         {
-            alphabetIndex = Random.Range(0, alphabets.Length);
+			alphabetIndex = Random.Range(0, alphabets.Length);
+			while(alphabetsDroped[alphabetIndex] == true){
+				Debug.Log ("In IEStart's while in while : " + gameObject.name);
+				alphabetIndex = Random.Range(0, alphabets.Length);
+			}
+			alphabetsDroped[alphabetIndex] = true;
+			cnt++;
+
             alphabetObject = Instantiate(alphabets[alphabetIndex], transform.position, transform.rotation) as GameObject;
             alphabetObject.GetComponent<Enemy>()._ICollisionObservable = _ICollisionObservableContainer;
             alphabetObject.GetComponent<EnemyMove>()._ICollisionObservableContainer = _ICollisionObservableContainer;
-            yield return new WaitForSeconds(Random.Range(1, 4));
+
+			if(cnt == alphabets.Length){
+				Debug.Log ("In IEStart's while in if : " + gameObject.name);
+				ResetArray ();
+				cnt = 0;
+			}
+
+			yield return new WaitForSeconds(Random.Range(1, 4));
         }
 	
 	}
 
+	void SetArray ()
+	{
+		Debug.Log ("In SetArray : " + gameObject.name);
+		alphabetsDroped = new bool[alphabets.Length];
+		ResetArray ();
+	}
+
+	void ResetArray ()
+	{
+		Debug.Log ("In ResetArray : " + gameObject.name);
+		for (int i = 0; i < alphabets.Length; i++)
+		{
+			alphabetsDroped[i] = false;
+		}
+	}
 }
