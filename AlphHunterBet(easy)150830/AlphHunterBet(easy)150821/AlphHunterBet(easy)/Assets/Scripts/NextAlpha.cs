c63fa09c0dc5e538fde7b8ca0bf38500f9e611a4
oitsmalab <oitsmalab@gmail.com>
2015-11-05 14:40:19 +0900

    残り数は削除、次のアルファベットを表示させるNextAlphaを作成しました。 hashimoto_stage3から確認できます。

---
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/NextAlpha.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextAlpha : MonoBehaviour {
+	public GameObject Alphabet;
+
+
+	public void Next(GameObject _alphabet){
+		Instantiate(_alphabet, this.transform.position, this.transform.rotation);
+	}
+}
