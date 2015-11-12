37845bc2d5dc1bd99c9695dbe48f0af27d809192
yanoty3d <vzb10326@me.com>
2015-11-12 18:56:12 +0900

    岡君のステージを入れた。状態遷移図において、最初のすてーとから、ミュートを使うことでBoss1,2,3で遷移できるようにした

--- NagaoOct2015/Assets/InBossStage1Boss1.cs
+++ NagaoOct2015/Assets/InBossStage1Boss1.cs
@@ -6,7 +6,7 @@ public class InBossStage1Boss1 : StateMachineBehaviour {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-		Application.LoadLevel("Boss2ISHIZAWA");
+		Application.LoadLevel("Boss1_Scene_Oka");
 
 
     }
