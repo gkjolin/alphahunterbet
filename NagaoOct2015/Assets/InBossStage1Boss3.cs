3dd9edf68195c795cd264e5247ddf0398e2a1c9d
yanoty3d <vzb10326@me.com>
2015-11-12 18:52:07 +0900

    Boss2, Boss3,石澤くんと橋本君のを追加

--- NagaoOct2015/Assets/InBossStage1Boss3.cs
+++ NagaoOct2015/Assets/InBossStage1Boss3.cs
@@ -6,7 +6,7 @@ public class InBossStage1Boss3 : StateMachineBehaviour {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Application.LoadLevel("Boss3");
+        Application.LoadLevel("Boss_hashimoto");
 
 
     }
