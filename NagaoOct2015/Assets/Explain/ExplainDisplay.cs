d4c02adcce4f705a3846eadff838265b7942502e
yanoty3d <vzb10326@me.com>
2015-11-13 17:20:52 +0900

    説明シーンで、クリックでスキップできる 「タッチしてね！」をついか プロダクト名にFをつけた

---
+++ NagaoOct2015/Assets/Explain/ExplainDisplay.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ExplainDisplay : MonoBehaviour {
+
+    int stageNumber;
+    Text _textRef;
+	// Use this for initialization
+	void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManagerScript.SetTrigger("ExplainFinished");            
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SceneManagerScript.SetTrigger("ExplainFinished");
+        }
+    }
+}
