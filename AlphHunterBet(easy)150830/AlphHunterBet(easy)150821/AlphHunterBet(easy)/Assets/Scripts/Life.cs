5e642f3afd85a1d0599b0d8914ab341e7dd8c698
oitsmalab <oitsmalab@gmail.com>
2015-11-24 18:32:25 +0900

    残機表示させるスクリプト。lifeに値を入れれば変化します。

---
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/Life.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Life : MonoBehaviour {
+	public Text _text;
+	public int life;
+
+	// Update is called once per frame
+	void Update () {
+		_text.text = "のこり" + life;
+	}
+}
