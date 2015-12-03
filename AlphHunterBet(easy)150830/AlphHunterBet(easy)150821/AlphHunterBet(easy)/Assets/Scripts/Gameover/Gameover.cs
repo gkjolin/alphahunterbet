f549469e50f7e4bafab2de6d8d15448a8d4fef15
yanoty3d <vzb10326@me.com>
2015-12-03 18:47:10 +0900

    #33 ボタンでゲームオーバーになるように変更。

--- AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/Gameover/Gameover.cs
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/Gameover/Gameover.cs
@@ -5,14 +5,16 @@ public class Gameover : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		if(Input.anyKey){　//何か入力があったとき
-			Application.LoadLevel("test"); //タイトルのシーンを呼ぶ
-		}
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Input.anyKey)
+        { //何か入力があったとき
+            Debug.Log("test");
+            //Application.LoadLevel("test"); //タイトルのシーンを呼ぶ
+        }
+
+    }
 }
