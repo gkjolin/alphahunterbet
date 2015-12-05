1fb29c557b6a0a0f07148992f761ab77dcc2b816
yanoty3d <vzb10326@me.com>
2015-12-05 17:07:07 +0900

    指でスプライトをドラッグするテスト。SpriteDrag.cs L24の５の値を変更すると、どれくらい正確に追いかけるかが変わる。

---
+++ DragMoveTest/Assets/SpriteDrag.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteDrag : MonoBehaviour
+{
+    public Sprite sprite;
+    private float z = 10f;
+
+    // Use this for initialization
+    void Start()
+    {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        renderer.sprite = sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnMouseDrag()
+    {
+        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, z));
+        transform.position = transform.position + (cursorPosition-transform.position)/5;
+    }
+}
