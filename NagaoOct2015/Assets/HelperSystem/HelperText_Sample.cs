99bdab2c3f7b464fdd0832f4fc37c5b65f5eceaf
oitsmalab <oitsmalab@gmail.com>
2015-12-08 15:35:45 +0900

    #47  Scriptsを修正しました。具体的には待機状態のテキスト表示とゲットしたときのテキスト表示が重ならないようにし、テキストの変更と表示を別のメソッドに分けて外部からでもStateを指定することで、Stateごとにテキスト内容を変更して表示できるようにしました。

--- NagaoOct2015/Assets/HelperSystem/HelperText_Sample.cs
+++ NagaoOct2015/Assets/HelperSystem/HelperText_Sample.cs
@@ -5,8 +5,8 @@ using System.Collections.Generic;
 
 public class HelperText_Sample : MonoBehaviour {
 
-	public Text text;
-	public string rightAlphabet;
+	public Text text;//text表示を行うためにUGUIのTextを格納
+	public string rightAlphabet;//正解のアルファベット
 
     [HideInInspector]
     public string getAlphabet;
@@ -17,85 +17,122 @@ public class HelperText_Sample : MonoBehaviour {
     public bool corutineStart;
 	
 	Dictionary<TEXTSTATE,string> showText=new Dictionary<TEXTSTATE,string>();
-
-	//sample text
+	
 	string success;
 	string miss;
 	string defeat;
 	string clear;
 	string wait;
-
-	// Use this for initialization
-	void Start () {
-		sampleTextSetting ();
-		textSet ();
-	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Start () {
+		SampleTextSetting ();
+		TextSet ();
 	}
-	public void textSet(){
+	/// <summary>
+	/// 初期テキスト
+	/// </summary>
+	void TextSet(){
 		showText.Add (TEXTSTATE.SUCCESS,success);
 		showText.Add (TEXTSTATE.MISS,miss);
 		showText.Add (TEXTSTATE.DEFEAT,defeat);
 		showText.Add (TEXTSTATE.WAIT,wait);
 		showText.Add (TEXTSTATE.CLEAR,clear);
 	}
-	void sampleTextSetting(){
+	void SampleTextSetting(){
 		success = "だな！正解だ！";
 		miss="は...間違いだな...";
 		defeat = "ぬわぁあああああああああああ！";
 		clear = "さぁ！次なる道へ行こう！";
 		wait = "はまだか...!?";
 	}
-
-	public void alphabetDicision(string alphabet){
+	/// <summary>
+	/// 正しいアルファベットか判定し、テキストを表示するようにする.
+	/// </summary>
+	/// <param name="alphabet">取得したAlphabet</param>
+	public void AlphabetDicision(string alphabet){
 		if (alphabet == rightAlphabet) {
-			showText[TEXTSTATE.SUCCESS]=getAlphabet+success;
 			textState=TEXTSTATE.SUCCESS;
 		} else {
-			showText[TEXTSTATE.MISS]=getAlphabet+miss;
 			textState=TEXTSTATE.MISS;
 		}
-		text.text=showText[textState];
-		StartCoroutine("WaitTimeCorutine");
+		TextChangeAndShow(textState);
 	}
+	/// <summary>
+	/// テキストを変更＆表示. ステートを選んで実行する
+	/// </summary>
+	/// <param name="textState">Text state.</param>
+	public void TextChangeAndShow(HelperText_Sample.TEXTSTATE textState){
+		switch (textState) {
 
+		case TEXTSTATE.SUCCESS://正解
+			StopCorutineMethod();
+			showText[textState]=getAlphabet+success;
+			StartCoroutine("WaitTimeCorutine");
+			break;
+		case TEXTSTATE.MISS://ミス
+			StopCorutineMethod();
+			showText[textState]=getAlphabet+miss;
+			StartCoroutine("WaitTimeCorutine");
+			break;
+		case TEXTSTATE.DEFEAT://ゲームオーバー
+			showText[textState]=defeat;
+			StopCorutineMethod();
+			break;
+		case TEXTSTATE.CLEAR://クリア
+			showText[textState]=clear;
+			StopCorutineMethod();
+			break;
+		case TEXTSTATE.WAIT://待機
+			showText[TEXTSTATE.WAIT]=rightAlphabet+wait;
+			corutineStart = false;
+			break;
+		}
+		text.text=showText[textState];//テキストを表示する
+	}
+	/// <summary>
+	/// 発生中のコルーチンを止める
+	/// </summary>
+	void StopCorutineMethod(){
+		corutineStart = false;
+		StopCoroutine("WaitTimeCorutine");
+	}
+	/// <summary>
+	/// とってからしばらくしてからお助けキャラが喋る
+	/// </summary>
+	/// <returns>The time corutine.</returns>
 	IEnumerator WaitTimeCorutine(){
-		if (corutineStart == true)yield break;
-		corutineStart = true;
+		if (corutineStart == true)yield break;//複数回コルーチンを呼び出さないようにする.
+		corutineStart = true;//コルーチン開始.
 		float time = 0;
-		Debug.Log ("corutine start");
 		while (time<4.5f) {
-			Debug.Log ("wait now");
 			time+=Time.deltaTime;
 			yield return null;
 		}
-		showText[TEXTSTATE.WAIT]=rightAlphabet+wait;
 		textState = TEXTSTATE.WAIT;
-		text.text=showText[textState];
-		corutineStart = false;
+		TextChangeAndShow (textState);
 		yield return new WaitForSeconds (2.0f);
 	}
 
-	//
-	public void showHelperText_SUCCESS(){
+
+	//----------------Demo用の表示関係--------------------------//
+	//Demoで実行する以外では関係なし
+	public void ShowHelperText_SUCCESS(){
 		textState= TEXTSTATE.SUCCESS;
 		text.text = showText [textState];
 	}
-	public void showHelperText_MISS(){
+	public void ShowHelperText_MISS(){
 		textState = TEXTSTATE.MISS;
 		text.text = showText [textState];
 	}
-	public void showHelperText_CLEAR(){
+	public void ShowHelperText_CLEAR(){
 		textState = TEXTSTATE.CLEAR;
 		text.text = showText [textState];
 	}
-	public void showHelperText_WAIT(){
+	public void ShowHelperText_WAIT(){
 		textState = TEXTSTATE.WAIT;
 		text.text = showText [textState];
 	}
-	public void showHelperText_DEFEAT(){
+	public void ShowHelperText_DEFEAT(){
 		textState = TEXTSTATE.DEFEAT;
 		text.text = showText [textState];
 	}
