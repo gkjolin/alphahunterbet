ea17a332935d96abeed4da283311541e526551c1
oitsmalab <oitsmalab@gmail.com>
2015-12-17 14:43:03 +0900

    #48 新しいParticleSystemの追加とコードを書き始めた.

---
+++ NagaoOct2015/Assets/Suzuki/Particle/BossExplosion.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossExplosion : MonoBehaviour {
+
+	[Tooltip("[int] Bossに持たせるParticleSystemの個数")]
+	public int explosionNum;
+	[Tooltip("[ParticleSystem] Explosionの役割を持つParticleSystemをいれる.")]
+	public ParticleSystem[] explosions;
+	[Tooltip("[ParticleSystem] 最後の演出に使うExplosion(finish)を入れる.")]
+	public ParticleSystem explosionF;
+
+	void Start (){
+		explosions = new ParticleSystem[explosionNum];
+	}
+}
