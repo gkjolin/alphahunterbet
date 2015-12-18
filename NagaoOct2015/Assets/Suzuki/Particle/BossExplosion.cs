34ed2bbd71da1739215ff0330c466a1d7c784808
oitsmalab <oitsmalab@gmail.com>
2015-12-18 12:13:30 +0900

    #48 前回の勉強会での指摘にあったパーティクルの色について変更を行う.

--- NagaoOct2015/Assets/Suzuki/Particle/BossExplosion.cs
+++ NagaoOct2015/Assets/Suzuki/Particle/BossExplosion.cs
@@ -3,14 +3,12 @@ using System.Collections;
 
 public class BossExplosion : MonoBehaviour {
 
-	[Tooltip("[int] Bossに持たせるParticleSystemの個数")]
-	public int explosionNum;
-	[Tooltip("[ParticleSystem] Explosionの役割を持つParticleSystemをいれる.")]
-	public ParticleSystem[] explosions;
+	[Tooltip("[ParticleSystem] Explosionの役割を持つParticleSystemをいれる.5個まで.")]
+	public ParticleSystem[] explosions = new ParticleSystem[5];
 	[Tooltip("[ParticleSystem] 最後の演出に使うExplosion(finish)を入れる.")]
 	public ParticleSystem explosionF;
 
-	void Start (){
-		explosions = new ParticleSystem[explosionNum];
+	void ExplosionStart (int arrayIndex){
+		explosions [arrayIndex].Play ();
 	}
 }
