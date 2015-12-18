6fff3c07a63e93352126d8c67a96a5c1352a3b1a
oitsmalab <oitsmalab@gmail.com>
2015-12-18 16:06:12 +0900

    #48 効果音に使う音を探した. また,パーティクルシステムの動作にかかわるコードを書いた.

--- NagaoOct2015/Assets/Suzuki/Particle/BossExplosion.cs
+++ NagaoOct2015/Assets/Suzuki/Particle/BossExplosion.cs
@@ -8,7 +8,26 @@ public class BossExplosion : MonoBehaviour {
 	[Tooltip("[ParticleSystem] 最後の演出に使うExplosion(finish)を入れる.")]
 	public ParticleSystem explosionF;
 
-	void ExplosionStart (int arrayIndex){
+	//前に再生したパーティクルシステムの添え字を保存する変数
+	private int index;
+	//
+	private bool flag = true;
+
+	public void ExplosionStart (int arrayIndex){
+		if (flag) {
+			flag = !flag;
+		} else {
+			explosions [index].Stop ();
+		}
+		index = arrayIndex;
 		explosions [arrayIndex].Play ();
+	}
+
+	public void FinishExplosionStart (){
+		explosionF.Play ();
+	}
+
+	public void BossHide (){
+
 	}
 }
