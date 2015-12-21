a8455b03c2debe7a5c72d6500e99ab4eed4c1690
oitsmalab <oitsmalab@gmail.com>
2015-12-21 16:24:45 +0900

    #50 背景の隕石の動きを縦向きの動きから右斜め上から左斜め下に動くように変更しました

--- AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/Background.cs
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/Background.cs
@@ -9,10 +9,11 @@ public class Background : MonoBehaviour
 	{
 		// 時間によってYの値が0から1に変化していく。1になったら0に戻り、繰り返す。
 		float y = Mathf.Repeat (Time.time * speed, 1);
-		
+		float x = Mathf.Repeat (Time.time * speed, 1);
+
 		// Yの値がずれていくオフセットを作成
-		Vector2 offset = new Vector2 (0, y);
-		
+		Vector2 offset = new Vector2 (x, y);
+
 		// マテリアルにオフセットを設定する
 		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", offset);
 	}
