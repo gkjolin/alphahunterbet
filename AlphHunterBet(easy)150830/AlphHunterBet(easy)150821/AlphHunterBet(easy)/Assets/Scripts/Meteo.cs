33c8271a9ceebdab906360e923c292068084e90d
oitsmalab <oitsmalab@gmail.com>
2016-01-18 18:19:57 +0900

    #50　背景の隕石をオブジェクトとして動くように変更。宇宙の絵は仮置き状態です

---
+++ AlphHunterBet(easy)150830/AlphHunterBet(easy)150821/AlphHunterBet(easy)/Assets/Scripts/Meteo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Meteo: MonoBehaviour
+{
+	// Spaceshipコンポーネント
+	Spaceship spaceship;
+
+	public int Des = 5;
+
+	void Start ()
+	{
+		
+		// Spaceshipコンポーネントを取得
+		spaceship = GetComponent<Spaceship> ();
+		
+		// ローカル座標のY軸のマイナス方向に移動する
+		spaceship.Move (transform.up * -1);
+
+		//3秒後にオブジェクトを削除
+		Destroy(gameObject, Des);
+	}
+}
